0fd8ad8e930484ccbcf193f3a03d481342420043
Digilevich Alexandr <stalcom90@gmail.com>
2016-03-30 23:02:55 +0300

    abs test

---
+++ HW/UnitTesting/MSTestExample.cs
@@ -0,0 +1,39 @@
+﻿namespace UnitTesting
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class MsTest
+    {
+
+        public TestContext TestContext { get; set; }
+
+        [TestMethod]
+        [DataSource("System.Data.OleDb",
+            "Provider=Microsoft.Jet.OLEDB.4.0;Data Source='I:\\Мои проекты в ASP.NET\\Epam\\automation testing\\1\\HW\\HW\\TestResults\\TestData.xls';Persist Security Info=False;Extended Properties='Excel 8.0'",
+            "Sheet1$", DataAccessMethod.Sequential)]
+        public void DataSourceTest()
+        {
+            string rowNumber = this.TestContext.DataRow["Number"].ToString();
+            string rowText = this.TestContext.DataRow["Text"].ToString();
+
+            Assert.IsNotNull(rowNumber);
+            Assert.IsNotNull(rowText);
+        }
+
+        [TestMethod]
+        public void SimpleStringAssert()
+        {
+            StringAssert.Contains("ABCD", "BC");
+        }
+        
+      [TestMethod]
+      public void SimpleTestExample()
+      {
+          Assert.IsTrue(true);
+      }
+
+
+
+  }
+}
---
+++ HW/UnitTesting/NUnitExample.cs
@@ -0,0 +1,58 @@
+﻿namespace UnitTesting
+{
+    using System;
+    using CSharpCalculator;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class NUnitExample
+    {
+        public Calculator Calc;
+
+        [SetUp]
+        public void SetUpInit()
+        {
+            this.Calc = new Calculator();
+        }
+
+        [TestCase(10, 7, ExpectedResult = 17)]
+        public double AddExampleTest(double a, double b)
+        {
+            return this.Calc.Add(a, b);
+        }
+
+        [TestCase(12, 3, 4)]
+        [TestCase(12, 2, 6)]
+        [TestCase(12, 4, 3)]
+        public void SimpleExampleTestCase(int n, int d, int q)
+        {
+            Assert.AreEqual(q, n / d);
+        }
+
+        [Test, Sequential]
+        public void TestDivisionBy2(
+            [Values(10, 25, 40)] int input,
+            [Values(5, 12, 20)] int expectedOutput)
+        {
+            Assert.AreEqual(expectedOutput, input / 2);
+        }
+
+        [Test]
+        public void RandomTest([Random(5)] double x)
+        {
+            Console.WriteLine("X = " + x);
+        }
+
+        [Test]
+        public void AbsTest()
+        {
+            var number = 11.9;
+            var result = 12;
+
+            var actual = Calc.Abs(number);
+
+            Assert.AreEqual(actual, result);
+        }
+    }
+}
+
