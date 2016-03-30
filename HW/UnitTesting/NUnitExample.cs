92de4d1e4e210f3f321569db645d95402614bf92
Digilevich Alexandr <stalcom90@gmail.com>
2016-03-30 23:33:42 +0300

    cos divide pow(fails) sqr sub isnegotive ispositive

--- HW/UnitTesting/NUnitExample.cs
+++ HW/UnitTesting/NUnitExample.cs
@@ -7,18 +7,18 @@
     [TestFixture]
     public class NUnitExample
     {
-        public Calculator Calc;
+        public Calculator _calc;
 
         [SetUp]
         public void SetUpInit()
         {
-            this.Calc = new Calculator();
+            _calc = new Calculator();
         }
 
         [TestCase(10, 7, ExpectedResult = 17)]
         public double AddExampleTest(double a, double b)
         {
-            return this.Calc.Add(a, b);
+            return _calc.Add(a, b);
         }
 
         [TestCase(12, 3, 4)]
@@ -49,9 +49,61 @@
             var number = 11.9;
             var result = 12;
 
-            var actual = Calc.Abs(number);
+            var actual = _calc.Abs(number);
 
             Assert.AreEqual(actual, result);
+        }
+
+        [TestCase(0,  ExpectedResult = 1)]
+        public double CosTest(int a)
+        {
+            return _calc.Cos(a);
+        }
+
+        [TestCase(12, 3, 4)]
+        [TestCase(12, 2, 6)]
+        [TestCase(12, 4, 3)]
+        public void DivideTest(int dividend, int divider, int quotient)
+        {
+            var actual = _calc.Divide(dividend, divider);
+
+            Assert.AreEqual(quotient ,actual);
+        }
+
+        [Test, Sequential]
+        public void Pow2Test(
+            [Values(10, 2, 5, 1)] double input,
+            [Values(100, 4, 25, 1)] double expectedOutput)
+        {
+            double two = 2;
+
+            var actual = _calc.Pow(input, two);
+
+            Assert.AreEqual(expectedOutput, input);
+        }
+
+        [Test]
+        public void SqrtTest()
+        {
+            Assert.AreEqual(2, _calc.Sqrt(4));
+        }
+
+        [Test]
+        public void SubTest()
+        {
+            Assert.AreEqual(3, _calc.Sub(4, 1));
+        }
+
+        [Test]
+        public void IsNegativeTest()
+        {
+            Assert.True( _calc.isNegative(-1));
+        }
+
+        [Test]
+        public void IsPositiveTest()
+        {
+            Assert.False(_calc.isPositive(-1));
         }
     }
 }
